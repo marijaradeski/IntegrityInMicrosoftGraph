e1faf9a785e18868925d3a9161d1241799a6b23d
user <user@DESKTOP-RNM89PG>
2026-06-21 15:17:24 +0200

    adding hash service and testing it

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿var basePath = Directory.GetCurrentDirectory();
+var filePath = Path.Combine(basePath, "testFile");
+
+File.WriteAllBytes(filePath, new byte[1024]);
+
+var newHashService = new IntegrityInMicrosoftGraph.Security.HashService();
+newHashService.ComputeHash(filePath);
+
+Console.WriteLine(newHashService.ComputeHash(filePath));
---
+++ Security/HashService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace IntegrityInMicrosoftGraph.Security
+{
+    public class HashService
+    {
+        public string ComputeHash(string filePath)
+        {
+            using var sha = SHA256.Create();
+            using var stream = File.OpenRead(filePath);
+            var hash = sha.ComputeHash(stream);
+            return Convert.ToHexString(hash);
+        }
+    }
+}
